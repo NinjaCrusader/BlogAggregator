556c5f4c02fce96f38357c1ecf782b5676c02075
NinjaCrusader <jeroldpemberton@gmail.com>
2026-02-03 19:35:56 -0500

    created a new commands struct in commands.go. Added a run method and register method to the commands struct.

--- commands.go
+++ commands.go
@@ -15,6 +15,27 @@ type command struct {
 	args []string
 }
 
+type commands struct {
+	commandMap map[string]func(*state, command) error
+}
+
+func (c *commands) run(s *state, cmd command) error {
+	if commandToRun, ok := c.commandMap[cmd.name]; ok {
+		err := commandToRun(s, cmd)
+		if err != nil {
+			return err
+		}
+	} else {
+		return fmt.Errorf("command doesn't exist")
+	}
+
+	return nil
+}
+
+func (c *commands) register(name string, f func(*state, command) error) {
+	c.commandMap[name] = f
+}
+
 func handlerLogin(s *state, cmd command) error {
 	if len(cmd.args) == 0 {
 		return fmt.Errorf("no username argument provided")
