54db5895c8be031b1d2d036091f28ba2bbd33cdd
NinjaCrusader <jeroldpemberton@gmail.com>
2026-02-09 21:52:12 -0500

    created a handlerRegister function to handle registering users to the db. update the login function to check the db to verify if that user exists within the db. registered the handlerRegister function within the main.go file.

--- commands.go
+++ commands.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"context"
+	"database/sql"
+	"errors"
 	"fmt"
+	"time"
 
 	"github.com/NinjaCrusader/BlogAggregator/internal/config"
 	"github.com/NinjaCrusader/BlogAggregator/internal/database"
+	"github.com/google/uuid"
+	"github.com/lib/pq"
 )
 
 type state struct {
@@ -43,9 +49,21 @@ func handlerLogin(s *state, cmd command) error {
 		return fmt.Errorf("usage: login <username>")
 	}
 
-	s.cfg.Username = cmd.args[0]
+	username := cmd.args[0]
 
-	if err := s.cfg.SetUser(cmd.args[0]); err != nil {
+	_, err := s.db.GetUser(context.Background(), username)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return fmt.Errorf("this user does not exist\n")
+		}
+
+		if dbError, ok := err.(*pq.Error); ok {
+			return fmt.Errorf("error with query: %v\n", dbError.Code)
+		}
+		return fmt.Errorf("error with query: %v\n", err)
+	}
+
+	if err := s.cfg.SetUser(username); err != nil {
 		return err
 	}
 
@@ -55,5 +73,33 @@ func handlerLogin(s *state, cmd command) error {
 }
 
 func handlerRegister(s *state, cmd command) error {
+
+	if len(cmd.args) < 1 {
+		return fmt.Errorf("no argument provided")
+	}
+
+	var userParams database.CreateUserParams
+
+	userParams.ID = uuid.New()
+	userParams.CreatedAt = time.Now()
+	userParams.UpdatedAt = time.Now()
+	userParams.Name = cmd.args[0]
+
+	createdUser, err := s.db.CreateUser(context.Background(), userParams)
+	if err != nil {
+		if dbError, ok := err.(*pq.Error); ok {
+			if dbError.Code == "23505" {
+				return fmt.Errorf("a user with this name already exists: %v\n", dbError)
+			}
+			return fmt.Errorf("error creating user: %v\n", dbError.Code)
+		} else {
+			return fmt.Errorf("error creating user: %v\n", err)
+		}
+	}
+
+	s.cfg.SetUser(createdUser.Name)
+
+	fmt.Printf("The user %v was created %v\n", createdUser.Name, createdUser)
+
 	return nil
 }
--- main.go
+++ main.go
@@ -35,6 +35,7 @@ func main() {
 	}
 
 	commands.register("login", handlerLogin)
+	commands.register("register", handlerRegister)
 
 	if len(os.Args) < 2 {
 		log.Fatalf("not enough arguments provided")
